cc072d9d4e2f1a2a16959489ec184590e575817b
fervovita <rrvvy9595@gmail.com>
2026-05-19 23:48:21 +0900

    [FEAT] 예약 좌석 엔티티에 Unique 제약 조건 추가

--- src/main/java/com/ceos/spring_cgv_23rd/domain/reservation/adapter/out/persistence/entity/ReservationSeatEntity.java
+++ src/main/java/com/ceos/spring_cgv_23rd/domain/reservation/adapter/out/persistence/entity/ReservationSeatEntity.java
@@ -1,25 +1,42 @@
 package com.ceos.spring_cgv_23rd.domain.reservation.adapter.out.persistence.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "reservation_seat")
+@Table(
+	name = "reservation_seat",
+	uniqueConstraints = {
+		@UniqueConstraint(columnNames = {"screening_id", "seat_id"})
+	})
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReservationSeatEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "reservation_seat_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "reservation_seat_id")
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reservation_id", nullable = false)
-    private ReservationEntity reservation;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "reservation_id", nullable = false)
+	private ReservationEntity reservation;
 
-    @Column(name = "seat_id", nullable = false)
-    private Long seatId;
+	@Column(name = "seat_id", nullable = false)
+	private Long seatId;
 }
