2e0efac53edb12dd680039d88e95fea6ead5731e
fervovita <rrvvy9595@gmail.com>
2026-05-21 14:22:04 +0900

    [FEAT] 예약 좌석 엔티티에 복합 인덱스 추가

--- src/main/java/com/ceos/spring_cgv_23rd/domain/reservation/adapter/out/persistence/entity/ReservationSeatEntity.java
+++ src/main/java/com/ceos/spring_cgv_23rd/domain/reservation/adapter/out/persistence/entity/ReservationSeatEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -16,7 +17,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "reservation_seat")
+@Table(
+	name = "reservation_seat",
+	indexes = {
+		@Index(
+			name = "idx_reservation_seat_reservation_id_seat_id",
+			columnList = "reservation_id, seat_id"
+		)
+	}
+)
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
