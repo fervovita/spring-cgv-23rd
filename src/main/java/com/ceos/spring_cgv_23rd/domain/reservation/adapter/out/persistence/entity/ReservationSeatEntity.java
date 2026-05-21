7835266db43d2d7a92546ee44c74596331e2a2ae
fervovita <rrvvy9595@gmail.com>
2026-05-21 13:45:36 +0900

    [REFACTOR] 예약 좌석 엔티티에 Unique 제약 조건 삭제

--- src/main/java/com/ceos/spring_cgv_23rd/domain/reservation/adapter/out/persistence/entity/ReservationSeatEntity.java
+++ src/main/java/com/ceos/spring_cgv_23rd/domain/reservation/adapter/out/persistence/entity/ReservationSeatEntity.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,11 +16,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(
-	name = "reservation_seat",
-	uniqueConstraints = {
-		@UniqueConstraint(columnNames = {"screening_id", "seat_id"})
-	})
+@Table(name = "reservation_seat")
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
